4e56da34dfba0d0b6bf27a7b39e0be8d373b46f9
Skooy <104994184+Skooyo@users.noreply.github.com>
2024-11-15 21:32:13 +0700

    styling fix

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useDynamicContext, useIsLoggedIn } from "@dynamic-labs/sdk-react-core";
+import { useState, useEffect } from "react";
+import { set } from "react-hook-form";
+
 export default function Home() {
+  const [connected, setConnected] = useState(false);
+  const { primaryWallet } = useDynamicContext();
+  // const isLoggedIn = useIsLoggedIn();
+
+  useEffect(() => {
+    if (primaryWallet) {
+      setConnected(true);
+    } else {
+      setConnected(false);
+    }
+  }, [primaryWallet]);
+
   return (
-    <div className="flex w-full h-full">
-      test
-    </div>
-  );
+    <>
+    {connected ? (
+      <div className="w-full h-screen flex-col flex items-center gap-4">
+        Wallet Connected!
+      </div>
+    ) : (
+      <div className="flex text-xl font-semibold w-full justify-center items-center">
+        <p>Please connect your wallet to use our features.</p>
+      </div>
+    )}
+  </>
+    );
 }
