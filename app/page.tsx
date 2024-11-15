36986661a6bb3c7fc20082a7456074073f5dd88f
Vincent <vincent.wesley.liem@gmail.com>
2024-11-15 23:06:20 +0800

    finish hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/landing-sections/Hero";
 import LandingHeader from "@/components/landing-sections/LandingHeader";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <LandingHeader />
+      <Hero />
     </main>
   );
 }
